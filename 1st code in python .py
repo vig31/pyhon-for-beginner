45dc795e0e3e1533854638b88b4513e681ad4cc3
vignesh136 <40334082+vignesh136@users.noreply.github.com>
2020-05-25 17:51:42 +0530

    Add files via upload

---
+++ 1st code in python .py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # my 1st code in python
+# 
+
+# In[14]:
+
+
+## data types 
+(a,b,c,d)=(2,3.4,'hello','world')
+print(type(a),type(b),type(c),type(d))
+## TYPE is used to tell what is it 
+## PRINT used for printing or displayig the content inside it 
+
+
+
+# In[16]:
+
+
+## list and tuple
+A =[1,2,'hi']
+B = (2,5,'friend')
+print(type(A),type(B))
+## [] is used to declare the nums or char & () is used to tuple
+
+
+# In[ ]:
+
+
+
+
